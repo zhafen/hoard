bec392de8629d0ca51b924a9042b33a68229e3a7
Zachary Hafen <zhafen@u.northwestern.edu>
2018-05-22 17:18:38 -0500

    Added part of the skeleton of the structure.

---
+++ hoard/treasure.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+'''Treausre objects, e.g. TreasureChests
+
+@author: Zach Hafen
+@contact: zachary.h.hafen@gmail.com
+@status: Development
+'''
+
+########################################################################
+########################################################################
+
+class TreasureChest( object ):
+
+    def __init__( self ):
+       pass 
+
+    ########################################################################
+
+    def generate_item( self ):
+
+        pass
